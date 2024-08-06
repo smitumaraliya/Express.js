dbdb3069606aa4d5771a55dc4d32e527ccef7c49
smitumaraliya <umaraliyasmeet12@gmail.com>
2024-08-06 18:43:40 +0530

    6-8-24

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express')
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.setHeader('Content-type', 'application/json')
+    res.send('Server Started...')
+    res.end()
+})
+
+server.get('/admin', (req, res) => {
+    res.setHeader('Content-type', 'application/json')
+    res.send('Server Started at Admin page...')
+    res.end()
+})
+
 server.listen(1111, () => {
     console.log("Server Started at http://127.0.0.1:1111");
 })
